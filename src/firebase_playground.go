de8b11dd3a1b0e0ffd24b2581d6fe34e9315fdd0
Shingo Ishimura <metal.tie@gmail.com>
2015-06-15 19:46:35 +0900

    firego使って、FirebaseのAPI叩いてみた

--- src/firebase_playground.go
+++ src/firebase_playground.go
@@ -1,6 +1,9 @@
 package firebase_playground
 
 import (
+	"appengine"
+	"fmt"
+	"github.com/CloudCom/firego"
 	"net/http"
 )
 
@@ -9,5 +12,12 @@ func init() {
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	// some code
+	c := appengine.NewContext(r)
+	f := firego.New("https://shining-heat-7158.firebaseio.com/")
+
+	var v map[string]interface{}
+	if err := f.Value(&v); err != nil {
+		c.Errorf("%v", err.Error())
+	}
+	fmt.Fprintf(w, "%s", v)
 }
