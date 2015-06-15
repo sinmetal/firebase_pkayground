fe2bf4c4d79a6aaa671e8f987d1ced435877e4c9
Shingo Ishimura <metal.tie@gmail.com>
2015-06-15 20:45:39 +0900

    gaeにPostした内容をFirebaseに書き込むようにした

--- src/firebase_playground.go
+++ src/firebase_playground.go
@@ -13,11 +13,38 @@ func init() {
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	c := appengine.NewContext(r)
-	f := firego.New("https://shining-heat-7158.firebaseio.com/")
+	f := firego.New("https://shining-heat-7158.firebaseio.com/chatlog")
 
-	var v map[string]interface{}
-	if err := f.Value(&v); err != nil {
-		c.Errorf("%v", err.Error())
+	if r.Method == "GET" {
+		var v map[string]interface{}
+		if err := f.Value(&v); err != nil {
+			c.Errorf("%s", err.Error())
+			fmt.Fprintf(w, "%s", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		fmt.Fprintf(w, "%s", v)
 	}
-	fmt.Fprintf(w, "%s", v)
+
+	if r.Method == "POST" {
+		v := map[string]string{"name": "gae", "text": r.FormValue("text")}
+		pushedFirego, err := f.Push(v)
+		if err != nil {
+			c.Errorf("%s", err.Error())
+			fmt.Fprintf(w, "%s", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		var result map[string]string
+		if err := pushedFirego.Value(&result); err != nil {
+			c.Errorf("%s", err.Error())
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprintf(w, "%s", err)
+			return
+		}
+
+		fmt.Fprintf(w, "%s: %s\n", pushedFirego, result)
+	}
+
 }
